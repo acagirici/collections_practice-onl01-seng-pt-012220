3da12c5d3a0ad1097314a7968cff19aa92a116b7
Arif Cagirici <acagirici23@gmail.com>
2020-02-01 23:25:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,6 +15,6 @@ def sort_array_char_count(strings)
 end
 
 def swap_elements(array)
-  array[1], array[2], 
+  array[1], array[2] = array [2], array[1]
 def swap_elements_from_to(array, index, destination_index)
   

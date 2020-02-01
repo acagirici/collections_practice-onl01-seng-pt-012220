9027d9b3e561dbec23d19490ddd9e2cc4dfad8c9
Arif Cagirici <acagirici23@gmail.com>
2020-02-01 23:29:51 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -23,3 +23,5 @@ def swap_elements_from_to(array, index, destination_index)
   array[index], array[destination_index] = array[destination_index], array[index]
   return array
 end
+
+reverse_array(integers)
